05008f15b22d8b8f605dfeb491b328a2691c3c49
Matheus Felipe <matheusfeliperibas@gmail.com>
2021-10-27 00:29:04 -0300

    feature calculate deposits, withdraws and total of transactions

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,26 @@
+import { useContext } from 'react'
+import { TransactionsContext } from '../../TransactionsContext';
+
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
 
-import { Container } from "./styles";
+import { Container, Total } from "./styles";
 
 export function Summary() {
+  const {transactions} = useContext(TransactionsContext)
+  
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, { deposits: 0, withdraws: 0, total: 0 })
   
   return (
     <Container>
@@ -13,22 +29,37 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$2000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$1060,00</strong>
+        <strong>-
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)}
+        </strong>
       </div>
-      <div className='highlight-background' >
+      <Total value={summary.total}>
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$940,00</strong>
-      </div>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
+      </Total>
     </Container>
   );
 }
--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
+interface TotalType {
+  value: number
+}
+
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 2rem;
   margin-top: -8rem;
 
-  div {
+  div:not(:last-child) {
     background: var(--shape);
     padding: 1.5rem 2rem;
     border-radius: 0.25rem;
@@ -25,10 +29,31 @@ export const Container = styled.div`
       font-weight: 500;
       line-height: 3rem;
     }
-
-    &.highlight-background {
-      background: var(--green);
-      color: #FFF;
-    }
   }
 `;
+
+const colors = {
+  green: '#33CC95',
+  red: '#E62E4D'
+}
+
+export const Total = styled.div<TotalType>`
+  background: ${props => props.value >= 0 ? colors.green : colors.red};
+  padding: 1.5rem 2rem;
+  border-radius: 0.25rem;
+  color: #FFF;
+  
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+    font-weight: 500;
+    line-height: 3rem;
+  }
+`
